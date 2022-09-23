e0419620384796c69b3826659560cf8ce2e4ab67
ASUS <ASUS@LAPTOP-2AC3EL6C>
2022-09-23 23:52:30 +0300

    c1

---
+++ Video34/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		Product product = new Product("x", "y", 1, 2, 3);
+//		product.setName("Laptop");
+//		product.setId(10); 
+//		product.setDescription("Asus Laptop"); 
+//		product.setPrice(5000);
+//		product.setStockAmount(5);
+		System.out.println(product.getName());
+		ProductManager productManager = new ProductManager();	
+		productManager.add(product);
+		productManager.add2(1, "x", "asd", 2, 200);
+	}
+}
